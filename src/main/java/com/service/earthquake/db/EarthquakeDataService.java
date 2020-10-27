f7fe84df29c4cf41154f3ba201d83b2aa0e9b9a1
Eric Balawejder <eric.balawejder@gmail.com>
2020-10-27 17:55:02 -0400

    Added getEarthquakes() method to DataService

--- src/main/java/com/service/earthquake/db/EarthquakeDataService.java
+++ src/main/java/com/service/earthquake/db/EarthquakeDataService.java
@@ -2,19 +2,59 @@ package com.service.earthquake.db;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.util.ISO8601DateFormat;
+import com.service.earthquake.entity.Earthquake;
 import com.service.earthquake.entity.RawEarthquake;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.ParseException;
+import java.util.Date;
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class EarthquakeDataService {
 
-    final private ObjectMapper objectMapper = new ObjectMapper();
-
     public List<RawEarthquake> getRawEarthquakeData(String filePath) throws IOException {
-        InputStream jsonInputStream = getClass().getResourceAsStream(filePath);
+        final InputStream jsonInputStream = getClass().getResourceAsStream(filePath);
+
+        final ObjectMapper objectMapper = new ObjectMapper();
 
         return objectMapper.readValue(jsonInputStream, new TypeReference<>() {});
+    }
+
+    public List<Earthquake> getEarthquakes() throws IOException {
+        final List<RawEarthquake> rawEarthquakes = getRawEarthquakeData("earthquake.json");
+
+        return rawEarthquakes.stream()
+                .map(mapRawEarthquakeToEarthquake)
+                .collect(Collectors.toUnmodifiableList());
+    }
+
+    private Function<RawEarthquake, Earthquake> mapRawEarthquakeToEarthquake
+            = rawEarthquake -> {
+
+        Earthquake earthquake = new Earthquake();
+        try {
+            earthquake.setTime(convertStringToDate(rawEarthquake.getTime()));
+            earthquake.setLatitude(rawEarthquake.getLatitude());
+            earthquake.setLongitude(rawEarthquake.getLongitude());
+            earthquake.setDepth(rawEarthquake.getDepth());
+            earthquake.setMagnitude(rawEarthquake.getMagnitude());
+            earthquake.setMagType(rawEarthquake.getMagType());
+            earthquake.setId(rawEarthquake.getId());
+            earthquake.setPlace(rawEarthquake.getPlace());
+            earthquake.setType(rawEarthquake.getType());
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return earthquake;
+    };
+
+    private Date convertStringToDate(String time) throws ParseException {
+        final ISO8601DateFormat dateFormat = new ISO8601DateFormat();
+        return dateFormat.parse(time);
     }
 }
