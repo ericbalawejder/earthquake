07ce32c10fd04329e4eac35dc4750d7b2766bb09
Eric Balawejder <eric.balawejder@gmail.com>
2020-10-27 15:42:46 -0400

    Parameterized getRawEarthquakeData()

--- src/main/java/com/service/earthquake/db/EarthquakeDataService.java
+++ src/main/java/com/service/earthquake/db/EarthquakeDataService.java
@@ -10,15 +10,10 @@ import java.util.List;
 
 public class EarthquakeDataService {
 
-    public static void main(String[] args) throws IOException {
-        EarthquakeDataService earthquakeDataService = new EarthquakeDataService();
-        System.out.println(earthquakeDataService.getRawEarthquakeData());
-    }
-
     final private ObjectMapper objectMapper = new ObjectMapper();
 
-    public List<RawEarthquake> getRawEarthquakeData() throws IOException {
-        InputStream jsonInputStream = getClass().getResourceAsStream("earthquake.json");
+    public List<RawEarthquake> getRawEarthquakeData(String filePath) throws IOException {
+        InputStream jsonInputStream = getClass().getResourceAsStream(filePath);
 
         return objectMapper.readValue(jsonInputStream, new TypeReference<>() {});
     }
