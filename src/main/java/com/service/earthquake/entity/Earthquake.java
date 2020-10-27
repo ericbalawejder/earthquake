233a8c9d47b8e4d8aae5329c7c85839bedde9abb
Eric Balawejder <eric.balawejder@gmail.com>
2020-10-27 16:34:12 -0400

    Created Earthquake entity

---
+++ src/main/java/com/service/earthquake/entity/Earthquake.java
@@ -0,0 +1,135 @@
+package com.service.earthquake.entity;
+
+import java.util.Date;
+import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize.Inclusion;
+
+@SuppressWarnings("deprecation")
+@JsonSerialize(include = Inclusion.NON_DEFAULT)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Earthquake {
+
+    private Date time;
+    private Double latitude;
+    private Double longitude;
+    private Double depth;
+    private Double magnitude;
+    private String magType;
+    private String id;
+    private String place;
+    private String type;
+
+
+    public Date getTime() {
+        return this.time;
+    }
+
+    public void setTime(Date time) {
+        this.time = time;
+    }
+
+    public Double getLatitude() {
+        return this.latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return this.longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
+    public Double getDepth() {
+        return this.depth;
+    }
+
+    public void setDepth(Double depth) {
+        this.depth = depth;
+    }
+
+    public Double getMagnitude() {
+        return this.magnitude;
+    }
+
+    public void setMagnitude(Double mag) {
+        this.magnitude = mag;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPlace() {
+        return this.place;
+    }
+
+    public void setPlace(String place) {
+        this.place = place;
+    }
+
+    public String getMagType() {
+        return this.magType;
+    }
+
+    public void setMagType(String magType) {
+        this.magType = magType;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Earthquake that = (Earthquake) o;
+        return Objects.equals(time, that.time) &&
+                Objects.equals(latitude, that.latitude) &&
+                Objects.equals(longitude, that.longitude) &&
+                Objects.equals(depth, that.depth) &&
+                Objects.equals(magnitude, that.magnitude) &&
+                Objects.equals(magType, that.magType) &&
+                Objects.equals(id, that.id) &&
+                Objects.equals(place, that.place) &&
+                Objects.equals(type, that.type);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(time, latitude, longitude, depth, magnitude, magType, id, place, type);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("***Earthquake***\n");
+        stringBuilder.append("time = " + time + "\n");
+        stringBuilder.append("latitude = " + latitude + "\n");
+        stringBuilder.append("longitude = " + longitude + "\n");
+        stringBuilder.append("depth = " + depth + "\n");
+        stringBuilder.append("magnitude = " + magnitude + "\n");
+        stringBuilder.append("magType = " + magType + "\n");
+        stringBuilder.append("id = " + id + "\n");
+        stringBuilder.append("place = " + place + "\n");
+        stringBuilder.append("type = " + type + "\n");
+        return stringBuilder.toString();
+    }
+}
+
